fd43b751a365f082e6c3a0370984ce831ab09f66
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-22 00:49:51 +0530

    dadtatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log  (3 + 3)
+
+console.log("zaid")
+
+let name = "zaid"
+let age = 20
+let isloggedin = false
+
+
+//number => 2 to power 53
+// bigiint 
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+comnsole.log(typeof null); //object
