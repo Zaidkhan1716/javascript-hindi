fa86fe5f552231e52a190aa91b5c95a9ebd62b0f
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-22 01:14:41 +0530

    Create 03_conversionoperation.js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,11 @@
+let score = zaid
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "abc"  => NaN
