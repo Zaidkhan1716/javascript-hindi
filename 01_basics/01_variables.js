e9dc54d4ae88395632baa2dc8aa2c4b3ee75570c
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-22 00:31:33 +0530

    add repo of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = "423322"
+let accountEmail ="zaidkhan@.com"
+var accountPassword = "212121"
+accountCity = "latur"
+let accountState ="mahrashtra"
+
+
+ accountEmail = "zaid@.com"
+ accountPassword = "212121"
+ accountCity = "pune"
+ accountState ="mahrashtra"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
