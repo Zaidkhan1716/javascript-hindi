c77e0eaa8623b7cfedf86f3b11c8825fe632bb83
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-26 00:27:06 +0530

    Update 03_conversionoperation.js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,46 @@
-let score = zaid
+//let score = zaid
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
-let valueInNumber =Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "abc"  => NaN
+// true => 1; false => 0
+
+let IsLoggedIn = "zaid"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "zaid" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************** Operatins ***************************//
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 = "hi"
+let str2 = " zaid "
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log("1" + "2")
+console.log(1 + 2)
