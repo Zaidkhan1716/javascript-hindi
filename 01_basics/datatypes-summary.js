79391c878cf75be2e557b41528372bd375b007ed
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-27 07:57:51 +0530

    Create datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// primitive 
+
+// 7 types : String, Number, Boolean,Null, Undefined,Symbol,BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+// Reference  (Non primitive)
+
+// Array, Object, Function
+const heros = ["Thor","Shaktiman","Tony" ];
+let myObj  ={
+    name: "zaid",
+    age: 20,
+}
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+ console.log(typeof myFunction)
