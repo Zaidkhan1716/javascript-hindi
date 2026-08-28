38b42430eeb8137ce02afaf7cc5c52842065b101
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-08-28 12:21:36 +0530

    Create 05_Strings.js

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,22 @@
+const name ="zaid"
+const repoCount ="20"
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('zaidkhan')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.lenggth);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+
+
