607e2873a02fa8f1aed54417756132381f005887
Zaidkhan1716 <zaidpathan434@gmail.com>
2026-09-01 05:40:38 +0530

    Update 05_Strings.js

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -17,6 +17,15 @@ console.log(gameName.indexOf('d'));
 const newString = gameName.substring(0,4)
 console.log(newString);
 
+const newStringOne ="    zaid   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const user = "I am a BCA 3 year student";
+ console.log(user.replace('3','2'));
+ console.log(user.includes('zaid'));
+ 
+
 
 
 
